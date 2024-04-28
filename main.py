e14e7fa9f880faa107d38259f38ed454b5f94c6d
saltcam <saltcam@uw.edu>
2024-04-27 18:41:38 -0700

    Finished and working model!

--- main.py
+++ main.py
@@ -12,9 +12,11 @@
 
 # code copied from TensorFlow tutorial
 
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 import tensorflow as tf
 print("TensorFlow version:", tf.__version__)
-
 
 mnist = tf.keras.datasets.mnist
 
@@ -23,18 +25,32 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
 # credit goes to https://medium.com/@mgazar/lenet-5-in-9-lines-of-code-using-keras-ac99294c8086 for code ideas
+# model = tf.keras.Sequential([
+#     tf.keras.layers.InputLayer(shape=(28, 28, 1)),
+#     tf.keras.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same', activation='tanh'),
+#     tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(14, 14)),
+#     tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='valid', activation='tanh'),
+#     tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(5, 5)),
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(units=120, activation='tanh'),
+#     tf.keras.layers.Dense(units=84, activation='tanh'),
+#     tf.keras.layers.Dense(units=10, activation='softmax')
+# ])
+
+# god save this video
+# https://www.youtube.com/watch?v=zwSXSltRhh0
+
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='valid', activation='relu', input_shape=(28, 28, 6)),
-    tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(14, 14)),
-    tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='same', activation='relu', input_shape=(10, 10, 16)),
-    tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(5, 5)),
+    tf.keras.layers.InputLayer(shape=(28, 28, 1)),
+    tf.keras.layers.Conv2D(filters=6, kernel_size=5, padding='same', activation='sigmoid'),
+    tf.keras.layers.AveragePooling2D(pool_size=2, strides=2),
+    tf.keras.layers.Conv2D(filters=16, kernel_size=5, padding='valid', activation='sigmoid'),
+    tf.keras.layers.AveragePooling2D(pool_size=2, strides=2),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(units=120, activation='relu'),
-    tf.keras.layers.Dense(units=84, activation='relu'),
+    tf.keras.layers.Dense(units=120, activation='sigmoid'),
+    tf.keras.layers.Dense(units=84, activation='sigmoid'),
     tf.keras.layers.Dense(units=10, activation='softmax')
-
 ])
-
 # model = tf.keras.models.Sequential([
 #     tf.keras.layers.Flatten(input_shape=(28, 28)),
 #     tf.keras.layers.Dense(128, activation='relu'),
